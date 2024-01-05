1c83d01f70955a3d44ddbaefe999d47936f04845
Tunae <tunae.santos82@gmail.com>
2024-01-05 18:55:17 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#campo-telefone").mask("(00) 00000-0000")
+
+    $("form").validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email:true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: "Por favor insira seu nome",
+            email: "Por favor insira seu email",
+            telefone: "Por favor insira seu telefone",
+            mensagem: "Por favor escreva sua mensagem"
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
